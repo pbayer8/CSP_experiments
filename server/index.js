fc97afeac9500df79c22a7643cf72f6e685efcb9
Philip Bayer <pbayer8@gmail.com>
2017-09-28 07:36:29 -0400

    scroll and edit works

--- server/index.js
+++ server/index.js
@@ -10,6 +10,43 @@ app.use(cors());
 //TODO: refactor into proper DB LOL!
 user_position = {}
 
+function callJSDOM(source, callback) {
+    jsdom.env(
+        source, ['../jquery-3.2.1.min.js'], // (*)
+        function(errors, window) { // (**)
+            process.nextTick(
+                function() {
+                    if (errors) {
+                        throw new Error("There were errors: " + errors);
+                    }
+                    callback(window);
+                }
+            );
+        }
+    );
+}
+
+function immediateText(node, text) {
+    if (text == null) {
+        try {
+
+            textVal = node.contents().filter(function() {
+                return this.nodeType == 3;
+            })[0].nodeValue;
+            console.log(textVal);
+            console.log(textVal.trim().length);
+        } catch (err) {
+            //TODO: proper error handling
+            textVal = " ";
+        }
+        return textVal.trim().length;
+    } else {
+        textVal = node.contents().filter(function() {
+            return this.nodeType == 3;
+        })[0].nodeValue = text;
+        return true;
+    }
+}
 //TODO: refactor into proper cloud storage maybe w/ DB lookup
 app.get('/proxy', function(req, res) {
     var dir = './scraped/' + req.query.name;
@@ -34,78 +71,56 @@ app.get('/proxy', function(req, res) {
 app.get('/edit', function(req, res) {
     console.log('edit');
     var dir = './scraped/' + req.query.name;
-    var fullDir = './scraped/' + req.query.name + '/index.html';
+    var localFullDir = './scraped/' + req.query.name + '/index.html';
+    var fullDir = './server/scraped/' + req.query.name + '/index.html';
     var url = 'http://' + req.query.name + '.com';
     var user = req.query.uuid;
-    var htmlSource = fs.readFileSync(fullDir, "utf8");
+    var text = '';
+    if (req.query.text)
+        text = req.query.text;
+    var htmlSource = fs.readFileSync(localFullDir, "utf8");
     var currNode = null;
-    if(user_position[user]==null)
-    	user_position[user] = {};
-    if(user_position[user].pos==null)
-    	user_position[user].pos=0;
+    if (user_position[user] == null) {
+        user_position[user] = {};
+        user_position[user].pos = 0;
+    }
     callJSDOM(htmlSource, function(window) {
         var $ = window.$;
         var items = window.document.body.getElementsByTagName('*');
         var len = items.length;
-        console.log(len+' visible items');
-        console.log('user:' + user_position[user].pos);
-        $(items[user_position[user].pos]).removeAttr('style');
-        do {
-            user_position[user].pos++;
+        if (user == 'clear') {
+            user_position[user].pos = 0;
+            do {
+                user_position[user].pos++;
+                currNode = $(items[user_position[user].pos]);
+                currNode.removeAttr('style');
+            } while (immediateText(currNode) == 0 && user_position[user].pos < len);
+            res.end(fullDir);
+        }
+        if (text.length) {
             currNode = $(items[user_position[user].pos]);
-            console.log(currNode);
-        } while (immediateText(currNode) == 0 && user_position[user].pos < len);
-        if (user_position[user].pos >= len)
-        	user_position[user].pos = 0;
-        //TODO: header hack!
-        console.log('user:' + user_position[user].pos);
-        //NOTE: script tag removal broken
+            immediateText(currNode, text);
+        } else {
+            $(items[user_position[user].pos]).removeAttr('style');
+            do {
+                user_position[user].pos++;
+                currNode = $(items[user_position[user].pos]);
+                console.log(currNode);
+            } while (immediateText(currNode) == 0 && user_position[user].pos < len);
+            if (user_position[user].pos >= len)
+                user_position[user].pos = 0;
+            //TODO: header hack!
+            //NOTE: script tag removal broken
+            $(items[user_position[user].pos]).css('color', '#F00');
+        }
         $('script#jsdom').remove();
-        $('script#jsdom').remove();
-        $(items[user_position[user].pos]).css('color', '#F00');
-        fs.writeFileSync(fullDir, $('html')[0].outerHTML);
-        res.end();
+        fs.writeFileSync(localFullDir, $('html')[0].outerHTML);
+        res.end(fullDir);
     });
 
-    function callJSDOM(source, callback) {
-        jsdom.env(
-            source, ['../jquery-3.2.1.min.js'], // (*)
-            function(errors, window) { // (**)
-                process.nextTick(
-                    function() {
-                        if (errors) {
-                            throw new Error("There were errors: " + errors);
-                        }
-                        callback(window);
-                    }
-                );
-            }
-        );
-    }
-
-    function immediateText(node, text) {
-        if (text == null) {
-            try {
-
-                textVal = node.contents().filter(function() {
-                    return this.nodeType == 3;
-                })[0].nodeValue;
-                console.log(textVal);
-                console.log(textVal.trim().length);
-            } catch (err) {
-                //TODO: proper error handling
-                textVal = " ";
-            }
-            return textVal.trim().length;
-        } else {
-            textVal = node.contents().filter(function() {
-                return this.nodeType == 3;
-            })[0].nodeValue = text;
-            return false;
-        }
-    }
-
 });
+
+
 
 app.get("*", function(req, res) {
     response.end("404!");
--- textbox.js
+++ textbox.js
@@ -8,61 +8,97 @@
         // The DOM is ready!
         setTextHeight();
         var input = $('#input');
-        var background = $('#background');
-        focustext = input;
-        focusCaret(focustext);
+        var droppedIn = false;
+        input.focusout(function() {
+            focusCaret(input);
+        });
+        focusCaret(input);
         var backframe = $('iframe#backframe');
         var word = '';
+        var freezeWord = '';
         var lastWord = '';
-        var currentNode = null;
+        var text = '';
+        var startSlice = 0;
         input.keydown(keyHandler);
-        background.keydown(keyHandler);
-        // input.bind('input propertychange', function() {
-        //     console.log(droppedIn);
-        //     if (!droppedIn) {
-        //         var text = input.val();
-        //         var words = text.split(/\W+/);
-        //         lastWord = word;
-        //         // console.log('lastword :' + word + ':');
-        //         word = words[words.length - 1];
-        //         if (!word.length)
-        //             word = words[words.length - 2];
-        //         if (lastWord != word) {
-        //             // console.log('word :' + word + ':');
-        //             $.get('http://philips-macbook-pro.local:8080/proxy?name=' + word, function(data) {
-        //                 focusCaret(input);
-        //                 console.log('data:' + data);
-        //                 backframe.attr('src', data);
-        //             });
 
-        //             // backframe.on('load', function() {
-        //             //     console.log('iframe loaded')
-        //             //     currentNode = backframe.contents().find('body');
-        //             //     console.log(backframe);
-        //             // });
-        //         }
-        //         background.val(text + text.slice(0, 10) + '\n\n\n\n');
-        //         if (input.length)
-        //             background.scrollTop(input.scrollTop());
-        //     }
+        input.bind('input propertychange', keyBinding);
 
-        // });
+        function keyBinding() {
+            text = input.val();
+            if (droppedIn) {
+                var replacement = text.slice(startSlice, text.length);
+                $.get("http://philips-macbook-pro.local:8080/edit?name=" + word + '&uuid=' + uuid + '&text=' + replacement, function(data) {
+                    focusCaret(input);
+                    backframe.attr('src', data);
+                });
+            } else {
+                var words = text.split(/\W+/);
+                lastWord = word;
+                word = words[words.length - 1];
+                if (!word.length)
+                    word = words[words.length - 2];
+                if (lastWord != word) {
+                    $.get('http://philips-macbook-pro.local:8080/proxy?name=' + word, function(data) {
+                        focusCaret(input);
+                        console.log('data:' + data);
+                        backframe.attr('src', data);
+                    });
+                }
+            }
+        };
+
+        function keyHandler(e) {
+            switch (e.keyCode) {
+                case 13:
+                    if (e.shiftKey) {
+                        //TODO drop in
+                        droppedIn = !droppedIn;
+                        if (droppedIn) {
+                            startSlice = text.length;
+                            freezeWord = word;
+                        } else {
+                            $.get("http://philips-macbook-pro.local:8080/edit?name=" + word + '&uuid=clear', function(data) {
+                                focusCaret(input);
+                                input.val(input.val() + ' ' + freezeWord);
+                            });
+                        }
+                        console.log('were in=' + droppedIn);
+
+                    } else {
+                        e.preventDefault();
+                    }
+                    break;
+                case 38: //up
+                case 40: //down
+                    if (droppedIn) {
+                        startSlice = text.length;
+                        console.log('edit');
+                        $.get("http://philips-macbook-pro.local:8080/edit?name=" + word + '&uuid=' + uuid, function(data) {
+                            backframe.attr('src', function(i, val) { return val; });
+                        });
+                    }
+                case 39: //right
+                case 37: //left
+                    e.preventDefault();
+                    break;
+                default:
+                    break;
+            }
+
+        }
     });
     var uuid = userID();
-    var droppedIn = false;
-    var focustext = null;
 
     function setTextHeight() {
         var height = $(window).height();
-        $('#input').outerHeight(height / 2);
-        $('#background').outerHeight(height);
+        $('#input').outerHeight(height);
     }
 
-    function focusCaret(textField) {
-        textField.focus();
-        val = $(textField).val();
-        $(textField).val('');
-        $(textField).val(val);
+    function focusCaret(input) {
+        input.focus();
+        val = input.val();
+        input.val('');
+        input.val(val);
     }
 
     function userID() {
@@ -70,56 +106,7 @@
             Math.random().toString(36).substring(2, 15);
     }
 
-    function keyHandler(e) {
-        switch (e.keyCode) {
-            case 13:
-                if (e.shiftKey) {
-                    //TODO drop in
-                    droppedIn = !droppedIn;
-                    if (droppedIn)
-                        focustext = background;
-                    else
-                        focustext = input;
-                    focusCaret(focustext);
-                    console.log('were in=' + droppedIn);
-                } else {
-                    e.preventDefault();
-                }
-                break;
-            case 38: //up
-            case 40: //down
-                if (droppedIn) {
-                    console.log('edit');
-                    $.get("http://philips-macbook-pro.local:8080/edit?name=" + word + '&uuid=' + uuid, function(data) {
-                        console.log('back!:');
-                        backframe.attr('src', function(i, val) { return val; });
-                    });
-
-                    // if (currentNode.children().length) {
-                    //     currentNode = currentNode.children()[0];
-                    // } else if (currentNode.next().length) {
-                    //     currentNode = currentNode.next()[0];
-                    // } else {
-                    //     currentNode = currentNode.parent();
-                    //     if (currentNode.next().length) {
-                    //         currentNode = currentNode.next()[0];
-                    //     }
-                    // }
-                    // console.log(currentNode.tagName)
-                }
-                // currentNode.css('background-color', 'red');
-            case 39: //right
-            case 37: //left
-                e.preventDefault();
-                break;
-            default:
-                break;
-        }
-
-    }
-    // The rest of the code goes here!
     $(window).resize(setTextHeight);
-
 
 }(window.jQuery, window, document));
 // The global jQuery object is passed as a parameter
